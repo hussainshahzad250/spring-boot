256c2f4d273dd918c2b5f8fa2692bec6fc828922
Shahzad Hussain <hussain.shahzad250@gmail.com>
2023-03-05 17:11:00 +0530

    Update Employee.java

--- spring-boot-file-download/src/main/java/com/sas/filedownload/entity/Employee.java
+++ spring-boot-file-download/src/main/java/com/sas/filedownload/entity/Employee.java
@@ -2,8 +2,14 @@ package com.sas.filedownload.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.util.Date;
+
 
 @Getter
 @Setter
@@ -19,4 +25,14 @@ public class Employee {
     private String email;
     private String mobile;
     private String designation;
+    
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_on", columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP", updatable = false)
+	private Date createdOn;
+	
+    @LastModifiedDate
+	@Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modified_on", columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+	private Date modifiedOn;
 }
